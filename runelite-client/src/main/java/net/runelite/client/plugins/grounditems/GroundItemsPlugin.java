279e4c88c70feb2e4136d6763d080b791e4bde1d
Ethan <ethan.hedrick@ndsu.edu>
2019-01-24 13:48:50 -0600

    Update GroundItemsPlugin.java

--- runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsPlugin.java
@@ -36,6 +36,7 @@ import static java.lang.Boolean.TRUE;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
