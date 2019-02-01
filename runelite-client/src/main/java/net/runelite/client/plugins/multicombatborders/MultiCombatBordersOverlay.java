446b39c7a92c10ebe4e40265fe189ec991d92d94
Ethan <richardkrowland@student.hartnell.edu>
2019-02-01 04:19:32 -0600

    Update MultiCombatBordersOverlay.java

--- runelite-client/src/main/java/net/runelite/client/plugins/multicombatborders/MultiCombatBordersOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/multicombatborders/MultiCombatBordersOverlay.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018, Mattias Cederlund <https://github.com/mattec92>
+ * Copyright (c) 2019, Ethan <https://github.com/wea1th>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
