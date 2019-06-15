d985e36581e7a5494337d4fdf67840c0fcbb0caa
Ethan <richardkrowland@student.hartnell.edu>
2019-06-15 08:09:42 -0500

    Update AnimationID.java

--- runelite-api/src/main/java/net/runelite/api/AnimationID.java
+++ runelite-api/src/main/java/net/runelite/api/AnimationID.java
@@ -216,12 +216,12 @@ public final class AnimationID
 	public static final int BLOWPIPE_ATTACK = 5061;
 
 	//Vorkath
-	public static final int VORKATH_ATTACK = 7952;
-	public static final int VORKATH_FIRE_BOMB_ATTACK = 7960;
-	public static final int VORKATH_ACID_ATTACK = 7957;
 	public static final int VORKATH_WAKE_UP = 7950;
 	public static final int VORKATH_DEATH = 7949;
 	public static final int VORKATH_SLASH_ATTACK = 7951;
+	public static final int VORKATH_ATTACK = 7952;
+	public static final int VORKATH_FIRE_BOMB_OR_SPAWN_ATTACK = 7960;
+	public static final int VORKATH_ACID_ATTACK = 7957;
 
 	// Tekton
 	public static final int TEKTON_ANVIL = 7475;
